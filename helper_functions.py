9c47fcb5a6ec2f4c611b42f154d518ab88dd99b6
Ines <inesanett@gmail.com>
2023-12-23 20:20:17 +0100

    Add latest versions of autoencoders, baseline, fourier

--- helper_functions.py
+++ helper_functions.py
@@ -19,3 +19,17 @@ def display_blur_sharp_and_pred_images(img_id, blur_list, sharp_list, pred_list)
     for l in listed:
         plt.imshow(l[img_id])
         plt.show()
+        
+def calc_mae(image1, image2):
+    mae = np.mean(np.abs((image1 - image2))) # Calculate pixel differences
+    return mae
+
+def calc_mse(image1, image2):
+    mse = np.mean((image1.astype("float") - image2.astype("float")) ** 2) 
+    return mse
+
+def load_picture_from_folder(filename, file_path):
+    picture = cv2.imread(os.path.join(file_path, filename))
+    return picture/255 # Pixels between 0 and 1
+
+
